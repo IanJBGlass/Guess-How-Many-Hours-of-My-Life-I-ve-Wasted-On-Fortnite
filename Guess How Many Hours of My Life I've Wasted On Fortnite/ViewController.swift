411059fd47517b09f9ec3efa240166de1776845c
Jacob Glass <jacobglass@MacBook-Pro-5.local>
2018-10-09 10:12:13 -0500

    Initial Commit

---
+++ Guess How Many Hours of My Life I've Wasted On Fortnite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guess How Many Hours of My Life I've Wasted On Fortnite
+//
+//  Created by Jacob Glass on 10/9/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
